3dce4f23139c114b8c61863b8586d2f65362f95e
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-26 15:52:45 -0700

    start seconds converter

---
+++ ch03/secondsConverter.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+
+/**
+ * Converts and input number of seconds to Hours, Minutes, Seconds output
+ */
+
+public class secondsConverter {
+
+    public static void main(String[] args) {
+
+    }
+}
