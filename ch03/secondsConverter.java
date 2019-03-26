189887660b3c98fb580621ea19c58b235f84ff54
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-26 16:03:59 -0700

    seconds converter

--- ch03/secondsConverter.java
+++ ch03/secondsConverter.java
@@ -1,4 +1,4 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 /**
  * Converts and input number of seconds to Hours, Minutes, Seconds output
@@ -7,6 +7,21 @@ import java.util.scanner;
 public class secondsConverter {
 
     public static void main(String[] args) {
+        int seconds;
+        int minutes;
+        int hours;
+        int remainingSeconds;
+        int remainingMinutes;
 
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How many seconds? ");
+        seconds = in.nextInt();
+        minutes = seconds / 60;
+        remainingSeconds = seconds % 60;
+        hours = minutes / 60;
+        remainingMinutes = minutes % 60;
+
+        System.out.printf("%d Hours, %d Minutes, %d Seconds\n", hours, remainingMinutes, remainingSeconds);
     }
 }
