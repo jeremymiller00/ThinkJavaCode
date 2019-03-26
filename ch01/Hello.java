461de395e1cf086fb384fd07ac3c55b78bfab170
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-26 15:33:02 -0700

    compile and run

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // generate some simple output
-        System.out.println("Hello, World!");
+        System.out.println("ello, World!");
     }
 
 }
