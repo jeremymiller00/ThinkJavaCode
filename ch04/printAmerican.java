55df140198747b87dce93f70d94dcc03f39e8b14
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-26 17:15:50 -0700

    add error handling

--- ch04/printAmerican.java
+++ ch04/printAmerican.java
@@ -29,12 +29,31 @@ public class printAmerican {
         Scanner in = new Scanner(System.in);
         // get data from user
         System.out.println("\nWhat is the month? \n");
+        if (! in.hasNextLine()) {
+            System.err.println("Error, try again...\n");
+            return;
+        }
         month = in.nextLine();
+
         System.out.println("\nWhat is the day of the week? \n");
+        if (! in.hasNextLine()) {
+            System.err.println("Error!.\n");
+            return;
+        }
         day = in.nextLine();
+
         System.out.println("\nWhat is the day of the month (1-31)? \n");
+        if (! in.hasNextInt()) {
+            System.err.println("Error, looks like this is not a number.\n");
+            return;
+        }
         date = in.nextInt();
+
         System.out.println("\nWhat is the year? \n");
+        if (! in.hasNextInt()) {
+            System.err.println("Error, looks like this is not a number.\n");
+            return;
+        }
         year = in.nextInt();
 
         // call print method
